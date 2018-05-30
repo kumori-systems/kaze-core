46e0f6f921c3d095777529fe7b6c87ac38146aab
Josep Maria Bernabé Gisbert <jbgisbert@kumori.systems>
2018-05-30 17:16:53 +0200

    Changed the dependency with runtime

--- build/src/runtime.js
+++ build/src/runtime.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("./utils");
-const runtime = require("runtime");
+const runtime = require("@kumori/runtime");
 const path = require("path");
 class Runtime {
     constructor(workspacePath, templatesPath) {
