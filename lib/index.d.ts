0f2598b7293bfae03bba2659e830afd350bab163
Josep Maria Bernabé Gisbert <jbgisbert@kumori.systems>
2018-05-31 11:26:10 +0200

    Exported some more classes and interfaces

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,8 @@ export { IError } from './interface';
 import { Workspace } from './workspace';
 export { StampConfig, configuration } from './utils';
 export { Deployment, DeploymentInstanceInfo, RegistrationResult } from '@kumori/admission-client';
+export { ComponentConfig } from './component';
+export { ServiceConfig } from './service';
+export { DeploymentConfig } from './deployment';
+export { RuntimeConfig } from './runtime';
 export declare const workspace: Workspace;
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ import { Workspace } from './workspace';
 import { Stamp } from './stamp';
 export { StampConfig, configuration } from './utils';
 export { Deployment, DeploymentInstanceInfo, RegistrationResult } from '@kumori/admission-client';
+export { ComponentConfig } from './component';
+export { ServiceConfig } from './service';
+export { DeploymentConfig } from './deployment';
+export { RuntimeConfig } from './runtime';
+
 
 export const workspace = new Workspace(new Component(), new Deployment()
   , new LocalStamp(), new Runtime(), new Service(), new Stamp());
